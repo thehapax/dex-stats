d02bd75b7a60674c800dea02361db6ebcc0e4401
thehapax <39811582+thehapax@users.noreply.github.com>
2019-10-04 23:15:50 -0700

    split into main and bts method file

--- bts_spread_mapper.py
+++ bts_spread_mapper.py
@@ -105,37 +105,3 @@ def plot_sequence(bts_df, title, bts_symbol, invert, bar_width, poll_time):
     plt.pause(poll_time)
     plt.draw()
 
-if __name__ == '__main__':
-    title = "Bitshares DEX"
-    bts_symbol = "OPEN.BTC/BTS"
-    output_file = "output_file.csv"
-    depth = 1
-    poll_time = 3  # time to wait before polling again
-    bar_width = 30
-    invert = False
-    enable_plot = True
-
-    bts_market = setup_bitshares_market(bts_symbol)
-    bts_df = get_ob_data(bts_market, depth, invert)
-
-    try:
-        # check length of output file
-        file_length = max(open(output_file, 'r'), key=len)
-    except FileNotFoundError as e:
-        #first line
-        bts_df = get_ob_data(bts_market, depth, invert)
-        append_to_file(bts_df.to_csv(header=True, index=False), output_file)
-
-    while True:
-        try:
-            bts_df = get_ob_data(bts_market, depth, invert)
-            append_to_file(bts_df.to_csv(header=False, index=False), output_file)
-            log.info(f'{title} {bts_symbol}:\n {bts_df}')
-
-            if enable_plot:
-                plot_sequence(bts_df, title, bts_symbol, invert, bar_width, poll_time)
-
-        except Exception as e:
-            log.error(e)
-            break
-
